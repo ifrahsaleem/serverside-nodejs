e1976700863ad57f7709880f9c9833a297a316db
Ifrah Saleem <ifrahsaleem123@gmail.com>
2020-06-13 22:25:32 +0500

    Mongoose Part 3

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -9,17 +9,33 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    Dishes.create({
-        name: 'Uthapizza',
-        description: 'Test'
+     Dishes.create({
+        name: 'Uthappizza',
+        description: 'test'
     })
     .then((dish) => {
         console.log(dish);
-        
-        return Dishes.find({}).exec();
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
 
         return Dishes.remove({});
     })
@@ -28,7 +44,7 @@ connect.then((db) => {
     })
     .catch((err) => {
         console.log(err);
-    });
+    }); 
 
     // var newDish = Dishes({
     //     name: 'Uthappizza',
